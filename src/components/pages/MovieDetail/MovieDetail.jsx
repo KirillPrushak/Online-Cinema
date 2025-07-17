65bba28935e54f9d614d7065004f32ed079e38d4
KIrillPrushak <kirillprushak@gmail.com>
2025-07-17 15:58:03 +0300

    add info for the selected film

--- src/components/pages/MovieDetail/MovieDetail.jsx
+++ src/components/pages/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,162 @@
+import { ArrowBack, Language, Movie } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  CircularProgress,
+  Grid,
+  Typography,
+} from '@mui/material';
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import {
+  useGetFilmQuery,
+  useGetSequelsAndPrequelsQuery,
+  useGetStaffQuery,
+} from '../../../services/kinopoiskApi';
+import ErrorMessage from '../../ui/ErrorMessage';
 
 function MovieDetail() {
-  return <div>MovieDetail</div>;
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  const responseFitm = useGetFilmQuery(id);
+  const responseSiquelsAndPrequels = useGetSequelsAndPrequelsQuery(id);
+  const responseStaff = useGetStaffQuery(id);
+
+  if (
+    responseFitm.isLoading ||
+    responseSiquelsAndPrequels.isLoading ||
+    responseStaff.isLoading
+  ) {
+    return (
+      <Box display="Flex" justifyContent="center" margin="auto">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (responseFitm.error || responseStaff.error) {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <>
+      <Grid container spacing={2} mt={2}>
+        <Grid size={{ xs: 4 }}>
+          <img
+            src={responseFitm.data.posterUrl}
+            alt={responseFitm.data.nameRu}
+            width="100%"
+          />
+        </Grid>
+        <Grid size={{ xs: 6 }}>
+          <Grid container gap="10px">
+            <Grid size={2}>
+              <Button
+                startIcon={<ArrowBack />}
+                size="large"
+                onClick={() => navigate(-1)}
+              />
+            </Grid>
+            <Grid alignContent="center">
+              {<Typography variant="h5">{responseFitm.data.nameRu}</Typography>}
+            </Grid>
+          </Grid>
+          <Grid container gap="10px">
+            <Grid size={6}>{<Typography>Год</Typography>}</Grid>
+            <Grid>
+              <Typography gutterBottom>{responseFitm.data.year}</Typography>
+            </Grid>
+
+            <Grid size={6}>{<Typography>Страна</Typography>}</Grid>
+            <Grid>
+              {responseFitm.data.countries.map(({ country }) => (
+                <Typography key={country}>{country}</Typography>
+              ))}
+            </Grid>
+
+            <Grid size={6}>{<Typography>Жанры</Typography>}</Grid>
+            <Grid>
+              {responseFitm.data.genres.map(({ genre }) => (
+                <Typography gutterBottom key={genre}>
+                  {genre}
+                </Typography>
+              ))}
+            </Grid>
+
+            <Grid size={6}>{<Typography>Режиссеры</Typography>}</Grid>
+            <Grid>
+              {responseStaff.data
+                .filter((el) => el.professionText === 'Режиссеры')
+                .map(({ nameRu }) => (
+                  <Typography key={nameRu}>{nameRu}</Typography>
+                ))}
+            </Grid>
+
+            <Grid size={6}>
+              <Typography>Время</Typography>
+            </Grid>
+            <Grid>
+              <Typography gutterBottom>
+                {responseFitm.data.filmLength} (мин)
+              </Typography>
+            </Grid>
+
+            <Grid size={12}>
+              <Typography gutterBottom>Описание</Typography>
+            </Grid>
+            <Grid size={12}>
+              <Typography>
+                {responseFitm.data.description
+                  ? responseFitm.data.description
+                  : 'Описание отсутствует'}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid size={{ xs: 2 }}>
+          <Typography variant="h6">В главных ролях</Typography>
+          {responseStaff.data
+            .filter((el) => el.professionText === 'Актеры')
+            .slice(0, 10)
+            .map(({ nameRu }) => (
+              <Typography gutterBottom key={nameRu}>
+                {nameRu}
+              </Typography>
+            ))}
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={2}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Grid item xs={12}></Grid>
+        <ButtonGroup variant="outlined" size="small">
+          <Button
+            target="_blank"
+            href={responseFitm.data.webUrl}
+            endIcon={<Language />}
+          >
+            Кинопоиск
+          </Button>
+          <Button
+            target="_blank"
+            href={`https://www.imdb.com/title//${responseFitm.data.imdbId}`}
+            endIcon={<Movie />}
+          >
+            IMDB
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </>
+  );
 }
 
 export default MovieDetail;
--- src/services/kinopoiskApi.js
+++ src/services/kinopoiskApi.js
@@ -55,6 +55,19 @@ export const kinopoiskApi = createApi({
         ),
       }),
     }),
+    getFilm: builder.query({
+      query: (id) => `/v2.2/films/${id}`,
+    }),
+
+    getSequelsAndPrequels: builder.query({
+      query: (id) => `/v2.1/films/${id}/sequels_and_prequels`,
+      transformResponse: (response) =>
+        response.map((el) => ({ ...el, kinopoiskId: el.filmId })),
+    }),
+
+    getStaff: builder.query({
+      query: (id) => `/v1/staff?filmId=${id}`,
+    }),
   }),
 });
 
@@ -62,4 +75,7 @@ export const {
   useGetFilmsTopQuery,
   useGetFilmsQuery,
   useGetGenresAndCountriesQuery,
+  useGetFilmQuery,
+  useGetSequelsAndPrequelsQuery,
+  useGetStaffQuery,
 } = kinopoiskApi;
